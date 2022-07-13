581a45e787b218879440bc9f0c314d85407556ba
salunkhegr1712 <salunkhegr19.comp@coep.ac.in>
2022-07-13 09:58:57 +0530

    ejs is added

--- app.js
+++ app.js
@@ -4,10 +4,24 @@ const express = require('express');
 const app=express();
 app.use(bp.urlencoded({extended:true}));
 
+// so as we added the bodyparser to file and the server is also created
+// so now add the ejs engine to the our server
+// we have to create a folder to maintain the data within it
+// we will store the ejs file there in that folder which will present in same heiarchical level that of 
+// app.js and index.html file
+app.set("view engine","ejs");
+
 app.listen(3000,function(){
   console.log("server is running at localhost:3000")
 });
 
 app.get("/",function(req,res){
-  res.send("hello world");
+
+  var c= new Date();
+  var f=c.getDate;
+  if(f===6 || f===7){
+    res.send("is is weekend");
+  }
+  res.send("It is working day");
+
 });

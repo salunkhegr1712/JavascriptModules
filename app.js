b716511ca82b464488ac9a6f0eaca2f5f62e5389
salunkhegr1712 <salunkhegr19.comp@coep.ac.in>
2022-07-14 11:18:16 +0530

    make it better

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ app.use(bp.urlencoded({
   extended: true
 }));
 // creating a empty array
-var item=[" Buy Food"," Cook Food"," Eat Food"];
-
+var item = [" Buy Food", " Cook Food", " Eat Food"];
+let work = [];
 
 // so as we added the bodyparser to file and the server is also created
 // so now add the ejs engine to the our server
@@ -37,33 +37,17 @@ function givedate() {
   return data;
 }
 
-function numb() {
-  var a = new Date();
-  return a.getDay();
-
-}
-
 var data1 = givedate();
-var c = numb();
-app.get("/", function(req, res) {
 
+// get request for the root
+app.get("/", function(req, res) {
+  res.render("todolist",{data:data1,element:item});
   // in date 0 stands for sunday and the 6 stand for saturday
 
   // console.log(data1, c);
+  // so as push request also goes from the get so we have send Object entry through render
 
-// so as push request also goes from the get so we have send Object entry through render
-  if (c === 6 || c === 0) {
-    res.render("todolist", {
-      data: data1,
-      element:item,
-    });
 
-  } else {
-    res.render("todolist", {
-      data: data1,
-      element:item,
-    });
-  }
   // so render will search for the views folder and search for  file name called the list from it
   // res.render("list",{day:d,wday:w,work:h});
   // we are parsing multiple values through render object and it has assign value to same varibles which you assign
@@ -74,17 +58,39 @@ app.get("/", function(req, res) {
   //   day: d,
   //   wday: w,
   //   work: h
-  // })
+  //
+});
+// get request for work
+app.get("/work", function(req, res) {
+  res.render("todolist", {
+    data: "My Works",
+    element: work,
+  });
 });
 
 app.post("/", function(req, res) {
 
-// we cant parse and send value directly through the post
-  var box=req.body.box;
-  console.log(box);
-  // adding value to the items Array
-  item.push(box);// appending new value to the array we will send that array to ejs
+  // we cant parse and send value directly through the post
 
-// redirecting to get request
-  res.redirect("/");
+  if(req.body.submit="My Works"){
+    work.push(req.body.box);
+    console.log(work);
+
+    res.redirect("/work");
+  }
+  else {
+    item.push(req.body.box);
+    res.redirect("/");
+  }
+//ANGELA LECTURE 271
+  // adding value to the items Array // appending new value to the array we will send that array to ejs
+
+
+  // redirecting to get request
+
 });
+
+// here we just assigned the value to the post and separate the work and the todolist in same file
+// then we checked for value in post
+// the render the result through redirect
+// we can do the same with two differnt ejs file but templating helps us here
